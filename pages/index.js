2c4bace53b47768cd832e95bff9629b191d79fe9
Raphael-GC <raphaelgc.i9@gmail.com>
2021-01-04 05:53:13 -0300

    First customization

--- pages/index.js
+++ pages/index.js
@@ -5,61 +5,40 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Raphael Carneiro</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://www.linkedin.com/in/raphaelgci9/"> my Page</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          I'm a Full Stack Web Developer {' '}
+          <code className={styles.code}>student at Rocketseat</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://github.com/Raphael-GC/Xradar" className={styles.card}>
+            <h3>Xradar &rarr;</h3>
+            <p>A project for trainning Node.js, React, React Native and Mongo DB.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="https://github.com/Raphael-GC/ecoleta" className={styles.card}>
+            <h3>Ecoleta &rarr;</h3>
+            <p>This project was done during the Rocktseat bootcamp - Next Level Week #1</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <p>Made with ❤ by 
+          <a href="https://github.com/Raphael-GC" target="_blank" rel="noopener noreferrer">Raphael Carneiro</a>
+          <a> | Cultive uma  {' '} <img src="/favicon.ico" alt="Lumus" className={styles.logo} /></a>
+        </p>  
       </footer>
-    </div>
+    </div> 
   )
 }
