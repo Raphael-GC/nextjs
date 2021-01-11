d128cc46c3970cd10096322df8c210ebf5975436
Raphael-GC <raphaelgc.i9@gmail.com>
2021-01-11 20:19:48 -0300

    Updated UI

--- pages/lumus.js
+++ pages/lumus.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          This is the <a>Lumus</a>
+          It's <a>Lumus</a>
         </h1>
 
         <p className={styles.description}>My green-pet {' '}
