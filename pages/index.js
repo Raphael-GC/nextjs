69992f9aba46223aa6583955210c3e9ed4f0eb5e
Raphael-GC <raphaelgc.i9@gmail.com>
2021-01-11 20:13:30 -0300

    Updated UI

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -15,29 +16,38 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>I'm a Developer {' '}
-          <code className={styles.animated} href="https://app.rocketseat.com.br/me/raphaelcarneiro">student at Rocketseat</code>
+          <a href="https://app.rocketseat.com.br/me/raphaelcarneiro" target="_blank" className={styles.animated}>student at Rocketseat</a>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://github.com/Raphael-GC/Xradar" className={styles.card}>
+          <a href="https://github.com/Raphael-GC/Xradar" target="_blank" className={styles.card}>
             <h3>Xradar &rarr;</h3>
             <p>A project for trainning Node.js, React, React Native and Mongo DB.</p>
           </a>
 
-          <a href="https://github.com/Raphael-GC/ecoleta" className={styles.card}>
+          <a href="https://github.com/Raphael-GC/ecoleta" target="_blank" className={styles.card}>
             <h3>Ecoleta &rarr;</h3>
             <p>This project was done during the Rocktseat bootcamp - Next Level Week #1</p>
           </a>
 
-        </div>
+          <a href="https://github.com/Raphael-GC/Skylab_JS" target="_blank" className={styles.card}>
+            <h3>My notepad (lab) &rarr;</h3>
+            <p>This project is a record of my learning that includes the main languages ​​and frameworks currently most used in web programming.</p>
+          </a>
 
-        
+          <a href="https://github.com/Raphael-GC/nextjs" target="_blank" className={styles.card}>
+            <h3>Next.js &rarr;</h3>
+            <p>This page has no HTML file. It was builded with only JS and CSS.</p>
+          </a>
+        </div>        
       </main>
 
       <footer className={styles.footer}>
         <p>With 💖 by
-          <a href="https://github.com/Raphael-GC" target="_blank" rel="noopener noreferrer"> Raphael Carneiro</a>
-          <a> | Grow a {' '} <img src="/favicon.ico" alt="Lumus" className={styles.logo} /> !</a>
+          <a href="https://github.com/Raphael-GC" target="_blank"> Raphael Carneiro </a> - Grow a {' '} 
+          <Link href="/lumus">
+            <a><img className={styles.logo} src="/favicon.ico" alt="Lumus" /></a>
+          </Link>!
         </p>  
       </footer>
     </div> 
---
+++ pages/lumus.js
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../styles/Lumus.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Raphael Carneiro</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          This is the <a>Lumus</a>
+        </h1>
+
+        <p className={styles.description}>My green-pet {' '}
+          <a> <img src="/favicon.ico"/>buxus sempervirens</a>
+        </p>
+      </main>
+      <footer>
+        <Link href="/">
+            <a>Back Home</a>
+        </Link>
+    </footer>  
+    </div>
+  )
+}
